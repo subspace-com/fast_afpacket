cfde8a3dff37bf376a9476a6576d94ed82a52577
Joe Williams <williams.joe@gmail.com>
2022-05-09 12:59:40 -0700

    make curl silent

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install -y linux-base
 apt-get install -y libpcap-dev
 
-curl -OL https://golang.org/dl/go1.17.9.linux-amd64.tar.gz
+curl -OLs https://golang.org/dl/go1.17.9.linux-amd64.tar.gz
 tar -C /usr/local -xvf go1.17.9.linux-amd64.tar.gz
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
